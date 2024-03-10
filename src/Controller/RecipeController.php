2d3b59e295454852ace943ede4c3bcb079c61dea
Antoine-NVE <antoinenavette@gmail.com>
2024-03-10 09:33:33 +0100

    Moteur de template Twig

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -12,12 +12,15 @@ class RecipeController extends AbstractController
     #[Route('/recette', name: 'recipe.index', requirements: ["id" => "\d+", "slug" => "[a-z0-9-]+"])]
     public function index(): Response
     {
-        return new Response("Recette");
+        return $this->render('recipe/index.html.twig');
     }
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ["id" => "\d+", "slug" => "[a-z0-9-]+"])]
     public function show(string $slug, int $id): Response
     {
-        return new Response("Recette : $slug");
+        return $this->render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id
+        ]);
     }
 }
