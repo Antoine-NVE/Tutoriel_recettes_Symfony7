f3ff5fa6ee73731fac67f6f75a41bc3848b159d3
Antoine-NVE <antoinenavette@gmail.com>
2024-03-17 15:02:49 +0100

    Formulaires imbriqués

--- src/DataFixtures/RecipeFixtures.php
+++ src/DataFixtures/RecipeFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Ingredient;
+use App\Entity\Quantity;
 use App\Entity\Recipe;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -21,6 +23,48 @@ class RecipeFixtures extends Fixture implements DependentFixtureInterface
     {
         $faker = Factory::create('fr_FR');
         $faker->addProvider(new Restaurant($faker));
+
+        $ingredients = array_map(fn (string $name) => (new Ingredient())
+            ->setName($name)
+            ->setSlug(strtolower($this->slugger->slug($name))), [
+            "Oignon",
+            "Ail",
+            "Tomate",
+            "Poivron",
+            "Pomme de terre",
+            "Carotte",
+            "Céleri",
+            "Champignon",
+            "Courgette",
+            "Aubergine",
+            "Poulet",
+            "Boeuf",
+            "Poisson",
+            "Crevettes",
+            "Pâtes",
+            "Riz",
+            "Huile d'olive",
+            "Vinaigre balsamique",
+            "Sel",
+            "Poivre"
+        ]);
+
+        $units = [
+            "c. à s.",
+            "c. à c.",
+            "tasse",
+            "verre",
+            "g",
+            "kg",
+            "L",
+            "ml",
+            "oz",
+            "pincée"
+        ];
+
+        foreach ($ingredients as $ingredient) {
+            $manager->persist($ingredient);
+        }
 
         $categoriesName = ['Plat chaud', 'Dessert', 'Entrée', 'Goûter'];
         foreach ($categoriesName as $categoryName) {
@@ -43,6 +87,13 @@ class RecipeFixtures extends Fixture implements DependentFixtureInterface
                 ->setDuration($faker->numberBetween(2, 60))
                 ->setCategory($this->getReference($faker->randomElement($categoriesName)))
                 ->setUser($this->getReference('USER' . $faker->randomNumber(1, 10)));
+
+            foreach ($faker->randomElements($ingredients, $faker->numberBetween(2, 5)) as $ingredient) {
+                $recipe->addQuantity((new Quantity())
+                    ->setQuantity($faker->numberBetween(1, 250))
+                    ->setUnit($faker->randomElement($units))
+                    ->setIngredient($ingredient));
+            }
             $manager->persist($recipe);
         }
 
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use App\Entity\Recipe;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -38,6 +39,18 @@ class RecipeType extends AbstractType
                 'empty_data' => ''
             ])
             ->add('duration')
+            ->add('quantities', CollectionType::class, [
+                'entry_type' => QuantityType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'entry_options' => [
+                    'label' => false
+                ],
+                'attr' => [
+                    'data-controller' => 'form-collection'
+                ]
+            ])
             ->add('save', SubmitType::class, [
                 'label' => "Envoyer"
             ])
